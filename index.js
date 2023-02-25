6111edea08e2525e155c8d14062a4743f70d0ff2
Mollika-Deb <mollika1551@gmail.com>
2023-02-25 17:17:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.ineyp7q.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/',(req, res)=>{
+    res.send("food server is running")
+})
+
+app.listen(port, ()=>{
+    console.log(`food serve is runing on ${port}`)
+})
